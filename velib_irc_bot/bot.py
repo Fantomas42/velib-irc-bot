d3c8760e903c44028066a293df3b3565b1510714
Fantomas42 <fantomas42@gmail.com>
2015-03-17 15:46:51 +0100

    Implement the bot

--- velib_irc_bot/bot.py
+++ velib_irc_bot/bot.py
@@ -1,7 +1,10 @@
 """
 Bot for Velib on IRC
 """
+from irc.strings import lower
 from irc.bot import SingleServerIRCBot
+
+from veliberator import __version__
 
 
 class VelibIRCBot(SingleServerIRCBot):
@@ -12,8 +15,34 @@ class VelibIRCBot(SingleServerIRCBot):
             "veliberator",
             "Assistant pour les stations velib")
 
-    def on_welcome(self, serv, ev):
-        serv.join("#velib")
+    def on_nicknameinuse(self, server, event):
+        server.nick(server.get_nickname() + '_')
+
+    def on_welcome(self, server, event):
+        server.join("#velib")
+
+    def on_privmsg(self, server, event):
+        self.do_command(event, event.arguments[0])
+
+    def on_pubmsg(self, server, event):
+        a = event.arguments[0].split(', ', 1)
+        if len(a) > 1 and (lower(a[0]) ==
+                           lower(self.connection.get_nickname())):
+            self.do_command(event, a[1].strip())
+        return
+
+    def do_command(self, event, cmd):
+        nick = event.source.nick
+        c = self.connection
+
+        if cmd == 'disconnect':
+            self.disconnect('I will be bike !')
+        elif cmd == 'die':
+            self.die('Road rage !')
+        elif cmd == 'version':
+            c.notice(nick, 'Veliberator v' + __version__)
+        else:
+            c.notice(nick, "Not understood: " + cmd)
 
 
 def cmdline():
